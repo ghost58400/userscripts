9db0d47d87cb2e0b07688141604582ac2c609054
Unknown <jojo58400@hotmail.fr>
2018-11-25 15:49:52 +0100

    vrdfs

--- jojo messenger.js
+++ jojo messenger.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         jojo messenger
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.1.1
 // @description  try to take over the world!
 // @author       jojo
 // @match        https://www.messenger.com/*

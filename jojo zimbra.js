f8e42a365e55500cd08916bbe11c4b772ea6235c
Unknown <jojo58400@hotmail.fr>
2018-11-28 17:59:50 +0100

    new model

---
+++ jojo zimbra.js
@@ -0,0 +1,26 @@
+// ==UserScript==
+// @name         jojo zimbra
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       jojo
+// @match        https://partage.insa-cvl.fr/*
+// @grant        none
+// @require      http://code.jquery.com/jquery-3.3.1.min.js
+// @require      https://cdnjs.cloudflare.com/ajax/libs/favico.js/0.3.10/favico.min.js
+
+// @require      https://raw.githubusercontent.com/ghost58400/userscripts/master/jojo_badge.js
+// ==/UserScript==
+
+(function () {
+    'use strict';
+
+    setInterval(loop, 1000);
+
+    function loop() {
+        let inboxFolderLabel = document.getElementById('zti__main_Mail__2_textCell');
+        let num = inboxFolderLabel.textContent.slice(inboxFolderLabel.textContent.indexOf('(')+1, -1);
+        JojoBadge.update(num);
+    }
+
+})();
--- jojo_badge.js
+++ jojo_badge.js
@@ -1,61 +1,29 @@
-// ==UserScript==
-// @name         jojo badge
-// @namespace    http://tampermonkey.net/
-// @version      0.1
-// @description  try to take over the world!
-// @author       jojo
-// @match        https://www.facebook.com/*
-// @match        https://www.messenger.com/*
-// @match        https://partage.insa-cvl.fr/*
-// @match        https://outlook.live.com/mail/*
-// @grant        none
-// @require      http://code.jquery.com/jquery-3.3.1.min.js
-// @require      https://cdnjs.cloudflare.com/ajax/libs/favico.js/0.3.10/favico.min.js
-// ==/UserScript==
 
 (function () {
-    'use strict';
 
-    window.jojoFavicon = new Favico({animation: 'none'});
-    window.jojoBadge = 0;
-    var numToUpdate = 5;
-    var updatesSkipped = 0;
-    setInterval(loop, 1000);
-
-    function loop() {
-        let newNum = getTitleCount();
-        if (window.jojoBadge !== newNum) {
-            numToUpdate = 5;
-        }
-        if (updatesSkipped >= 4){
-            numToUpdate = 1;
-        }
-        if (numToUpdate > 0){
-            window.jojoFavicon.badge(newNum);
-            console.log('badge updated to ' + newNum);
-            numToUpdate = numToUpdate - 1;
-            updatesSkipped = 0;
-            window.jojoBadge = newNum;
-        }
-        else {
-            updatesSkipped = updatesSkipped + 1;
-        }
-    }
-    
-    function getTitleCount() {
-        let num = 0;
-        if (window.location.href.includes('https://partage.insa-cvl.fr/') ){
-            let inboxFolderLabel = document.getElementById('zti__main_Mail__2_textCell');
-            num = inboxFolderLabel.textContent.slice(inboxFolderLabel.textContent.indexOf('(')+1, -1);
-        }
-        else {
-            num = parseInt(document.title.replace(/^\D+/g, ''), 10);
-            if (isNaN(num)){
-                num = 0;
+    var JojoBadge = {
+        favicon : new Favico({animation: 'none'}),
+        actualBadge : 0,
+        numToUpdate : 5,
+        updatesSkipped : 0,
+        update : function (newNum) {
+            if (this.actualBadge !== newNum) {
+                this.numToUpdate = 5;
+            }
+            if (this.updatesSkipped >= 4){
+                this.numToUpdate = 1;
+            }
+            if (this.numToUpdate > 0){
+                this.favicon.badge(newNum);
+                console.log('real badge updated to ' + newNum);
+                this.numToUpdate--;
+                this.updatesSkipped = 0;
+                this.actualBadge = newNum;
+            }
+            else {
+                this.updatesSkipped++;
             }
         }
-
-        return num;
-    }
+    };
 
 })();

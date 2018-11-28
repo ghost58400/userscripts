947e7fdb1cab0092618a2a6d64bbb48a813a82f1
Unknown <jojo58400@hotmail.fr>
2018-11-28 18:02:30 +0100

    fsrgrs

--- jojo_badge.js
+++ jojo_badge.js
@@ -1,26 +1,25 @@
 
 (function () {
 
-    var JojoBadge = {
-        favicon : new Favico({animation: 'none'}),
-        actualBadge : 0,
-        numToUpdate : 5,
-        updatesSkipped : 0,
-        update : function (newNum) {
+    window.JojoBadge = {
+        favicon: new Favico({animation: 'none'}),
+        actualBadge: 0,
+        numToUpdate: 5,
+        updatesSkipped: 0,
+        update: function (newNum) {
             if (this.actualBadge !== newNum) {
                 this.numToUpdate = 5;
             }
-            if (this.updatesSkipped >= 4){
+            if (this.updatesSkipped >= 4) {
                 this.numToUpdate = 1;
             }
-            if (this.numToUpdate > 0){
+            if (this.numToUpdate > 0) {
                 this.favicon.badge(newNum);
                 console.log('real badge updated to ' + newNum);
                 this.numToUpdate--;
                 this.updatesSkipped = 0;
                 this.actualBadge = newNum;
-            }
-            else {
+            } else {
                 this.updatesSkipped++;
             }
         }

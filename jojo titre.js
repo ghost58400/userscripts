29b1d2d7d5b817493b1cd0a9648d90badf6cdeeb
Unknown <jojo58400@hotmail.fr>
2018-11-28 18:19:47 +0100

    gtsh

--- jojo titre.js
+++ jojo titre.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         jojo badge
+// @name         jojo titre
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  try to take over the world!
